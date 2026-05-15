490c240fb8f94e085b2499a236906d5a2f61b093
davywibowo <davywibowo10@gmail.com>
2026-05-15 15:38:20 +0700

    fix:bug fixed excel

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,27 +1,20 @@
-// import { supabase } from "@/lib/supabase"
-// import SensorCard from "@/components/sensorCards"
+import { supabase } from "@/lib/supabase";
+import SensorCard from "@/components/sensorCards";
 
-// export default async function Dashboard() {
+export default async function Dashboard() {
+  const { data } = await supabase
+    .from("soil_monitor")
+    .select("*")
+    .order("created_at", { ascending: false })
+    .limit(20);
 
-//   const { data } = await supabase
-//     .from("soil_monitor")
-//     .select("*")
-//     .order("created_at", { ascending:false })
-//     .limit(20)
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>Smart Irrigation Dashboard</h1>
 
-//   return (
-//     <div style={{padding:"40px"}}>
-
-//       <h1>Smart Irrigation Dashboard</h1>
-
-//       {data?.map((item) => (
-//         <SensorCard
-//           key={item.id}
-//           nilai={item.nilai}
-//           status={item.status}
-//         />
-//       ))}
-
-//     </div>
-//   )
-// }
+      {data?.map((item) => (
+        <SensorCard key={item.id} nilai={item.nilai} status={item.status} />
+      ))}
+    </div>
+  );
+}
